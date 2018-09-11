10b2a24beeb670bceee52812246442744fc06a60
sya2 <sya2@phs007736.partners.org>
2018-09-11 14:16:11 -0400

    added lat and lon. added ggmap package.

--- R/02_data_manipulation.R
+++ R/02_data_manipulation.R
@@ -6,15 +6,35 @@ baseline <- read_sas("data/raw_data/analysis_file_20121231.sas7bdat",
 
 copd_pre1 <- baseline %>% 
   mutate_at(vars(contains('Days_To_Onset')), funs(date_exac = (. + Rand_Date))) %>% # add days to onset to rand date to get sas date for each exacerbation
-  select(ID, contains("Severity_EX"), contains("clinic"), latitude, Rand_Date, contains("_date_exac")) %>% # select only the severity, location and date information
+  select(ID, contains("Severity_EX"), contains("clinic"), Rand_Date, contains("_date_exac")) %>% # select only the severity, clinic and date information
   rename_at(vars(contains("Days_To_Onset")), # rename date of exacerbation variables
             funs(sub("Days_To_Onset","dateofexac",.))) %>%
   rename_at(vars(contains("_date_exac")), 
-            funs(sub("_date_exac","",.))) 
+            funs(sub("_date_exac","",.))) %>% 
+  mutate(latitude = case_when(clinic=="A" ~ 39.2904, # add latitude
+                              clinic=="B" ~ 33.5186,
+                              clinic=="C" ~ 42.3601,
+                              clinic=="D" ~ 39.7392,
+                              clinic=="E" ~ 34.0522,
+                              clinic=="F" ~ 44.9778,
+                              clinic=="G" ~ 37.7749,
+                              clinic=="H" ~ 42.2808,
+                              clinic=="I" ~ 39.9526,
+                              clinic=="J" ~ 40.4406)) %>% 
+  mutate(longitude = case_when(clinic=="A" ~ -76.6122, # add longitude
+                               clinic=="B" ~ -86.8104,
+                               clinic=="C" ~ -71.0589,
+                               clinic=="D" ~ -104.9903,
+                               clinic=="E" ~ -118.2437,
+                               clinic=="F" ~ -93.2650,
+                               clinic=="G" ~ -122.4194,
+                               clinic=="H" ~ -83.7430,
+                               clinic=="I" ~ -75.1652,
+                               clinic=="J" ~ -79.9959))
 
 # split into date and severity datasets
-exac_dates_wide <- copd_pre1 %>% select(ID, clinic, nclinic, dateofexac1:dateofexac11)
-exac_severity_wide <- copd_pre1 %>% select(ID, clinic, nclinic, Severity_EX1:Severity_EX11)
+exac_dates_wide <- copd_pre1 %>% select(ID, clinic, nclinic, latitude, longitude, dateofexac1:dateofexac11)
+exac_severity_wide <- copd_pre1 %>% select(ID, clinic, nclinic, latitude, longitude, Severity_EX1:Severity_EX11)
 
 # convert wide format date and severity datasets to long
 exac_dates_long <- exac_dates_wide %>% gather(exacnumber, date, dateofexac1:dateofexac11)
@@ -36,7 +56,7 @@ exac_long <- exac_long %>%
   mutate(date = as.Date(date, origin='1960-01-01')) %>% 
   arrange(clinic) %>% # sort by date
   add_count(clinic) %>% # add a count by clinic
-  rename(n_in_center = n) %>% # rename n variable
+  rename(n_in_center = n) # rename n variable
   
 
 # add list of all months between start (2006-03-17) and end (2010-04-02)
@@ -56,5 +76,6 @@ exac_long_full <- exac_long_full %>%
   mutate(percent_exac = exacyesno/n_in_center*100) %>% 
   mutate(percent_exac = ifelse(is.na(percent_exac),0,percent_exac)) # add percentage of that center with exacerbations on that day
 
+# 
 
 
--- R/99_misc_code.R
+++ R/99_misc_code.R
@@ -32,5 +32,7 @@ copd_pre5 <- cbind(copd_pre4,
 ## sample plotting counts of exacerbations by date
 ggplot(data=remove_missing(exac_long_full, na.rm=TRUE), aes(x=quarter, y=percent_exac, color=clinic)) + stat_summary(fun.y = "sum", geom="smooth")
 
-                                                            
-                                                     
+
+## get map of united states
+map1 <- get_map(location='united states',zoom = 4, maptype="terrain", source='google')
+ggmap(map1)                                                   
